8a42c461bfe0447f7ba6e8a3f095f73c5cb9feef
René Faßbender <rene-fassb@gmx.de>
2023-06-02 12:12:53 +0200

    bugfix: list

--- src/App.js
+++ src/App.js
@@ -5,37 +5,41 @@ import { uid } from "uid";
 import ListDisplay from "./components/list/List";
 import useLocalStorageState from "use-local-storage-state";
 
-const testArray = [{ id: uid(), name: "test", goodWeather: "test" }];
-
 function App() {
-  const [activities, setActivities] = useLocalStorageState(testArray);
+  const [activities, setActivities] = useLocalStorageState("activities", []);
 
   function handleAddActivity(activity) {
-    setActivities([
-      ...activities,
-      {
-        id: uid(),
-        name: activity.name,
-        goodWeather: activity.goodWeather,
-      },
-    ]);
-    console.log("activities:" + activities);
+    const newActivity = {
+      id: uid(),
+      name: activity.name,
+      goodWeather: activity.goodWeather,
+    };
+
+    setActivities((prevActivities) => {
+      const updatedActivities = prevActivities ? [...prevActivities] : [];
+      updatedActivities.push(newActivity);
+      return updatedActivities;
+    });
   }
+
+  const activitiesList =
+    activities !== null && activities !== undefined ? activities : [];
 
   return (
     <>
       <Form onAddActivity={handleAddActivity} />
 
       <ul>
-        {activities.map((activity) => {
-          return <ListDisplay key={activity.id} activity={activity.name} />;
-        })}
+        {activitiesList.map((activity) => (
+          <ListDisplay key={activity.id} activity={activity.name} />
+        ))}
       </ul>
     </>
   );
 }
 
 export default App;
+
 /*
 import "./App.css";
 import Form from "./components/form/Form";
