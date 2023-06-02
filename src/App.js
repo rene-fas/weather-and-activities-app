53c5f6bebcb119ec7f7632bc740d95e98691a0a8
René Faßbender <rene-fassb@gmx.de>
2023-06-02 11:41:27 +0200

    feat: list bug:localstorage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,75 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Form from "./components/form/Form";
+import { useState } from "react";
+import { uid } from "uid";
+import ListDisplay from "./components/list/List";
+import useLocalStorageState from "use-local-storage-state";
+
+const testArray = [{ id: uid(), name: "test", goodWeather: "test" }];
+
 function App() {
+  const [activities, setActivities] = useLocalStorageState(testArray);
+
+  function handleAddActivity(activity) {
+    setActivities([
+      ...activities,
+      {
+        id: uid(),
+        name: activity.name,
+        goodWeather: activity.goodWeather,
+      },
+    ]);
+    console.log("activities:" + activities);
+  }
+
   return (
     <>
-      <Form></Form>
+      <Form onAddActivity={handleAddActivity} />
+
+      <ul>
+        {activities.map((activity) => {
+          return <ListDisplay key={activity.id} activity={activity.name} />;
+        })}
+      </ul>
     </>
   );
 }
 
 export default App;
+/*
+import "./App.css";
+import Form from "./components/form/Form";
+import { useState } from "react";
+import { uid } from "uid";
+import ListDisplay from "./components/list/List";
+
+function App() {
+  const [activities, setActivities] = useState([]);
+
+  function handleAddActivity(activity) {
+    const newActivity = {
+      id: uid(),
+      name: activity.name,
+      goodWeather: activity.goodWeather,
+    };
+
+    setActivities([...activities, newActivity]);
+    console.log(activities);
+  }
+
+  return (
+    <>
+      <Form onAddActivity={handleAddActivity} />
+
+      <ul>
+        {activities.map((activity) => {
+          return <ListDisplay key={activity.id} activity={activity.name} />;
+        })}
+      </ul>
+    </>
+  );
+}
+
+export default App;
+
+ */
--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,12 +1,22 @@
-export default function form() {
+export default function form({ onAddActivity }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+
+    console.log(data);
+    onAddActivity(data);
+    event.target.reset();
+    //event.target.focus("name"); //check later
+  }
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>Add new activity</h1>
         <label htmlFor="name">Name:</label>
         <input type="text" name="name"></input>
-        <label htmlFor="activity">Good weather activity</label>
-        <input type="checkbox" name="activity"></input>
+        <label htmlFor="goodWeather">Good weather activity</label>
+        <input type="checkbox" name="goodWeather"></input>
         <button type="submit">Submit</button>
       </form>
     </>
